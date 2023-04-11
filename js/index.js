310f5c8b961c9bd2fe0af3c33be1fe49916db335
DevloperTarek <devlopertarek433@gmail.com>
2023-04-11 09:06:02 +0600

    dizme project

---
+++ js/index.js
@@ -0,0 +1,24 @@
+let types =  new Typed(".nime",{
+     strings:["Bangladesh.","Bangladesh.","Bangladesh."],
+     typeSpeed:100,
+     backSpeed:65,
+     loop:true,
+ });
+ 
+ let type = new Typed(".nice",{
+    strings:["Designer.","Youtuber.","Devloper.","Engineer."],
+    typeSpeed:100,
+    backSpeed:50,
+    loop:true,
+ });
+//   let like = new Typed(".style",{
+//     strings:["desh.","desh.",],
+//     typeSpeed:100,
+//     backSpeed:80,
+//     delay:12,
+//     loop:true,
+//  });
+function say (message="tarek vai"){
+   console.log(message);
+}
+say();
